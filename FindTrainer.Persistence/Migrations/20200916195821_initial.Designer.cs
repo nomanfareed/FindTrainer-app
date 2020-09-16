797f327206b25d94fd32b09be2ee8cac9b86f594
Yazan Ati <yazan7ati@outlook.com>
2020-09-16 23:00:32 +0300

    updating database file

--- FindTrainer.Persistence/Migrations/20200916195821_initial.Designer.cs
+++ FindTrainer.Persistence/Migrations/20200916195821_initial.Designer.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FindTrainer.Persistence.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20200916173520_initial")]
+    [Migration("20200916195821_initial")]
     partial class initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
