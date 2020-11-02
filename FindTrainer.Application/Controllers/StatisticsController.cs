ae35c06787d380b34caa7f905406123c3d47e5c6
astutebytes <73800544+astutebytes@users.noreply.github.com>
2020-11-02 22:52:29 +0500

    Update StatisticsController.cs
    Added trainer stats view API which is accessible by Trainers only

--- FindTrainer.Application/Controllers/StatisticsController.cs
+++ FindTrainer.Application/Controllers/StatisticsController.cs
@@ -43,5 +43,54 @@ namespace FindTrainer.Application.Controllers
 
             return Ok(new { SigninCount = count });
         }
+        
+        [HttpGet("TrainerViews")]
+        [Authorize(Roles = "Trainer")]
+        public async Task<IActionResult> GetTrainerViews()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return Unauthorized("You are not authorized to access this page");
+            }
+
+            var prevDate = DateTime.Now.AddDays(-7).Date;
+
+            var dataQuery = await _statsQuery.Query
+                        .Where(x => x.TrainerId == CurrentUserId && x.DateAdded.Date > prevDate)
+                        .GroupBy(grp => grp.DateAdded.Date)
+                        .Select(y => new
+                        {
+                            Date = y.Key,
+                            Views = y.Sum(x => x.Counter)
+                        })
+                        .ToListAsync();
+
+            var dateTimes = new List<DateTime>();
+            for (int i = 0; i < 7; i++)
+            {
+                dateTimes.Add(DateTime.Now.Date.AddDays(-i));
+            }
+
+            var emptyTableQuery = from dt in dateTimes
+                                  select new
+                                  {
+                                      dt.Date,
+                                      Count = 0
+                                  };
+
+            var result = from e in emptyTableQuery
+                         join data in dataQuery on e.Date equals data.Date into g
+                         from finalData in g.DefaultIfEmpty()
+                         select new UserForStatsDto
+                         {
+                             TrainerId = CurrentUserId,
+                             ViewDate = e.Date,
+                             Count = finalData == null ? 0 : finalData.Views
+                         };
+
+            result = result.OrderByDescending(x => x.ViewDate);
+
+            return Ok(result);
+        }
     }
 }
