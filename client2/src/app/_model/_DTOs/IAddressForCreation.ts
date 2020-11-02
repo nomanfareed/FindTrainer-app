21ee1f227514281bd773d6ff6357ee37fbc72a3e
eric-programming <eric09279@outlook.com>
2020-11-01 22:54:44 -0800

    task init

---
+++ client2/src/app/_model/_DTOs/IAddressForCreation.ts
@@ -0,0 +1,6 @@
+export interface IAddressForCreation {
+  City: string;
+  Country: string;
+  Province: string;
+  Address: string;
+}
--- client2/src/app/_model/_Dto/MessageDTO.ts
+++
@@ -1,9 +0,0 @@
-export class MessageDTO {
-  trainerId: string;
-  id: string;
-  name: string = 'Name';
-  message: string = 'Message';
-  email: string = 'email@email.com';
-  phoneNumber: string = '7789996663';
-  title: string = 'Title';
-}
