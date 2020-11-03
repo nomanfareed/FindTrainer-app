67d94589339e844b41171df80c238ecb6571f435
eric-programming <eric09279@outlook.com>
2020-11-03 09:55:23 -0800

    Add Trainer dashboard account

--- client2/src/app/pages/dashboard/dashboard-trainer/dashboard-trainer-account/dashboard-trainer-account.component.ts
+++ client2/src/app/pages/dashboard/dashboard-trainer/dashboard-trainer-account/dashboard-trainer-account.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  BaseUserUpdateForReturn,
+  TrainerUpdateForReturn,
+} from 'src/app/_model/_DTOs/IUserUpdate';
 
 @Component({
   selector: 'app-dashboard-trainer-account',
   templateUrl: './dashboard-trainer-account.component.html',
-  styleUrls: ['./dashboard-trainer-account.component.css']
+  styleUrls: ['./dashboard-trainer-account.component.css'],
 })
 export class DashboardTrainerAccountComponent implements OnInit {
+  BaseUserAccountData: BaseUserUpdateForReturn = new BaseUserUpdateForReturn();
+  TrainerAccountData: TrainerUpdateForReturn = new TrainerUpdateForReturn();
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  ChangeProfileImage($event): void {
+    const finalData = $event; //This is the file for the photo;
   }
-
+  ChangeAccountSetting($event): void {
+    const finalData = $event as BaseUserUpdateForReturn; // change user account data
+  }
+  ChangeTrainerAccount($event): void {
+    const finalData = $event as TrainerUpdateForReturn; // change user account data
+  }
 }
