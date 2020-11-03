5c00813a212d6f34943e2dc1753340551966013b
eric-programming <eric09279@outlook.com>
2020-11-02 16:44:57 -0800

    adding admin

--- client2/src/app/_data/_sideBarContent.ts
+++ client2/src/app/_data/_sideBarContent.ts
@@ -1,43 +1,93 @@
 import { Role } from '../_model/_Enum/Role';
 import {
-  _dashboardStats_route,
-  _editAccount_route,
-  _editCertification_route,
+  _Dashboard_Account_route,
+  _Dashboard_Certs_route,
+  _Dashboard_Messages_route,
+  _Dashboard_Review_route,
+  _Dashboard_Stats_route,
+  _mainAdmin_Route,
   _maintrainer_route,
   _mainUser_route,
-  _myMessages_route,
 } from './_route';
 function properURL(role: Role, url: string) {
-  return role === Role.trainer
-    ? _maintrainer_route + '/' + url
-    : _mainUser_route + '/' + url;
+  if (role === Role.user) {
+    return `${_mainUser_route}/${url}`;
+  }
+  if (role === Role.trainer) {
+    return `${_maintrainer_route}/${url}`;
+  }
+  if (role === Role.admin) {
+    return `${_mainAdmin_Route}/${url}`;
+  }
 }
 export const _UserSideboard = [
   {
     name: 'Account',
     icon: 'fas fa-user-circle',
-    route: properURL(Role.user, _editAccount_route),
+    route: properURL(Role.user, _Dashboard_Account_route),
+  },
+  {
+    name: 'Messages',
+    icon: 'fas fa-envelope',
+    route: properURL(Role.user, _Dashboard_Messages_route),
+  },
+  {
+    name: 'Reviews',
+    icon: 'fas fa-star',
+    route: properURL(Role.user, _Dashboard_Review_route),
   },
 ];
 export const _TrainerSideBoard = [
   {
     name: 'Statics',
     icon: 'fas fa-chart-bar',
-    route: properURL(Role.trainer, _dashboardStats_route),
-  },
-  {
-    name: 'Account',
-    icon: 'fas fa-user-circle',
-    route: properURL(Role.trainer, _editAccount_route),
+    route: properURL(Role.trainer, _Dashboard_Stats_route),
   },
   {
     name: 'Certifications',
     icon: 'fas fa-school',
-    route: properURL(Role.trainer, _editCertification_route),
+    route: properURL(Role.trainer, _Dashboard_Certs_route),
   },
   {
     name: 'Messages',
     icon: 'fas fa-envelope',
-    route: properURL(Role.trainer, _myMessages_route),
+    route: properURL(Role.trainer, _Dashboard_Messages_route),
+  },
+  {
+    name: 'Reviews',
+    icon: 'fas fa-star',
+    route: properURL(Role.trainer, _Dashboard_Review_route),
+  },
+  {
+    name: 'Account',
+    icon: 'fas fa-user-circle',
+    route: properURL(Role.trainer, _Dashboard_Account_route),
+  },
+];
+export const _AdminSideBoard = [
+  {
+    name: 'Statics',
+    icon: 'fas fa-chart-bar',
+    route: properURL(Role.admin, _Dashboard_Stats_route),
+  },
+  {
+    name: 'Certifications',
+    icon: 'fas fa-school',
+    route: properURL(Role.trainer, _Dashboard_Certs_route),
+  },
+  {
+    name: 'Messages',
+    icon: 'fas fa-envelope',
+    route: properURL(Role.trainer, _Dashboard_Messages_route),
+  },
+  {
+    name: 'Reviews',
+    icon: 'fas fa-star',
+    route: properURL(Role.trainer, _Dashboard_Review_route),
+  },
+  {
+    name: 'Account',
+    icon: 'fas fa-user-circle',
+    route: properURL(Role.trainer, _Dashboard_Account_route),
   },
 ];
--- client2/src/app/_model/_Enum/Role.ts
+++ client2/src/app/_model/_Enum/Role.ts
@@ -1,5 +1,6 @@
 export enum Role {
   user = 0,
   trainer = 1,
+  admin = 2,
 }
 //we have two rules for our users.
