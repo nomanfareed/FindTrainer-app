8267d92dc2a459a0d6593fe6fadc443839532fb0
eric-programming <eric09279@outlook.com>
2020-11-02 22:48:35 -0800

    update account

--- client2/src/app/components/forms/account-user-form/account-user-form.component.ts
+++ client2/src/app/components/forms/account-user-form/account-user-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Gender } from 'src/app/_model/_Enum/Gender';
 
 @Component({
   selector: 'app-account-user-form',
@@ -6,10 +9,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-user-form.component.css']
 })
 export class AccountUserFormComponent implements OnInit {
+  @Input() fullData: TrainerDTO | BaseUserDTO;
+  AccountForm: FormGroup;
+  initData: UpdateAccountDTO = {
+    name: '',
+    gender: Gender.male,
+  };
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private fb: FormBuilder,
+    private toastr: ToastrService
+  ) {}
+  ngOnChanges() {
+    this.initForm();
+  }
+  initForm() {
+    if (this.fullData) {
+      Object.assign(this.initData, this.fullData);
+    }
+    this.AccountForm = this.fb.group({
+      name: [this.initData.name, [Validators.required]],
+      gender: [this.initData.gender, [Validators.required]],
+    });
+  }
 
-  ngOnInit(): void {
+  submitFunc(): void {
+    //Compare
+    const updatedData = { ...this.fullData, ...this.AccountForm.value };
+    const oldData = this.fullData;
+    if (JSON.stringify(oldData) === JSON.stringify(updatedData)) {
+      this.toastr.error('Please change the data before save, thanks!');
+    } else {
+      this.userService
+        .EditUser(updatedData)
+        .then(() => {
+          this.toastr.info('Account information is saved');
+        })
+        .catch((error) => {
+          this.toastr.error(error.message);
+        });
+    }
   }
 
 }
