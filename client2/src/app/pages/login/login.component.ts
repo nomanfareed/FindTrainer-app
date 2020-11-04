a8a2d764ea70cf88ebb6de9a2e81cbc35ec5af38
astutebytes <73800544+astutebytes@users.noreply.github.com>
2020-11-05 00:06:49 +0500

    Update login.component.ts

--- client2/src/app/pages/login/login.component.ts
+++ client2/src/app/pages/login/login.component.ts
@@ -1,31 +1,35 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import {
-  IUserForLoginDto,
-  UserForLoginDto,
-} from 'src/app/_model/_DTOs/IUserForLoginDto';
+import { AuthService } from './../../_services/_authS/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
-  constructor(private fb: FormBuilder) {}
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  constructor(private authService: AuthService, private fb: FormBuilder) {}
   isLoading: boolean = false;
-  initData: IUserForLoginDto = new UserForLoginDto();
 
   readonly emailOnly = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
-  loginForm = this.fb.group({
-    Email: [
-      this.initData.Email,
+  ngOnInit() {
+  this. loginForm = this.fb.group({
+    email: [
+      'lola@test.com',
       [Validators.pattern(this.emailOnly), Validators.required],
     ],
-    Password: [this.initData.Password, [Validators.required]],
+    password: ['P@ssw0rd', [Validators.required]],
   });
+  }
 
   submitFunc() {
-    const finalData: IUserForLoginDto = this.loginForm.value;
+    const finalData = this.loginForm.value;
+
+    this.authService.login(finalData).subscribe(() => {
+    });
   }
 }
