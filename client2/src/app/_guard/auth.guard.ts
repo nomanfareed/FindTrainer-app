93e47ca2370f144130ef086f288a27582a18db18
astutebytes <73800544+astutebytes@users.noreply.github.com>
2020-11-04 23:53:49 +0500

    updated authguard

--- client2/src/app/_guard/auth.guard.ts
+++ client2/src/app/_guard/auth.guard.ts
@@ -1,28 +1,33 @@
-// import { Injectable } from '@angular/core';
-// import {
-//   CanActivate,
-//   ActivatedRouteSnapshot,
-//   RouterStateSnapshot,
-//   UrlTree,
-// } from '@angular/router';
-// import { ToastrService } from 'ngx-toastr/toastr/toastr.service';
-// import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { ToastrService } from 'ngx-toastr/toastr/toastr.service';
+import { Observable } from 'rxjs';
+import { _login_route } from '../_data/_route';
+import { AuthService } from '../_services/_authS/auth.service';
 
-// @Injectable({
-//   providedIn: 'root',
-// })
-// export class AuthGuard implements CanActivate {
-//   constructor(
-//     private accountService: AccountService,
-//     private toastr: ToastrService
-//   ) {}
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
-//   canActivate(): Observable<boolean> {
-//     return this.accountService.currentUser$.pipe(
-//       map((user) => {
-//         if (user) return true;
-//         this.toastr.error('You shall not pass!');
-//       })
-//     );
-//   }
-// }
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+                        Observable<boolean> | Promise<boolean> | boolean {
+        if (this.authService.isAuthenticated()) {
+            return true;
+        }
+
+        this.router.navigate([_login_route], { queryParams: { returnUrl: state.url } });
+        return false;
+    }
+}
