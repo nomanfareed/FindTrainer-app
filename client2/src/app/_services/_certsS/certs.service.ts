7b9395354750f60898e801bc4f1c2292962d3a4d
astutebytes <73800544+astutebytes@users.noreply.github.com>
2020-11-05 19:22:57 +0500

    Update certs.service.ts

--- client2/src/app/_services/_certsS/certs.service.ts
+++ client2/src/app/_services/_certsS/certs.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { _api_addCert, _api_certList, _api_deleteCert } from 'src/app/_data/_apiRoute';
+import { ICertificationForCreationDto } from 'src/app/_model/_DTOs/ICertificationForCreationDto';
+import { environment } from 'src/environments/environment';
+import { AuthService } from '../_authS/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CertsService {
 
-  constructor() { }
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+ // headerObject = new HttpHeaders().set('Authorization', 'Bearer' + localStorage.getItem('token'));
+
+  addCert(model: ICertificationForCreationDto) {
+    return this.http.post(this.baseUrl + _api_addCert, model);
+  }
+
+  deleteCert(certId: number) {
+    return this.http.delete(this.baseUrl + _api_deleteCert + '/' + certId);
+  }
+
+  // tslint:disable-next-line: typedef
+  listCert(trainerId: number, pageNumber: number) {
+
+    const params = new HttpParams().set('trainerId', trainerId.toString()).set('pageNumber', pageNumber.toString());
+
+    return this.http.get(this.baseUrl + _api_certList, {params});
+  }
+
 }
