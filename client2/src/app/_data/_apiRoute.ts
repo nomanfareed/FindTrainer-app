6dff788cd639905023d0f8cb21c63638b82a027b
astutebytes <73800544+astutebytes@users.noreply.github.com>
2020-11-05 19:23:31 +0500

    Update _apiRoute.ts

--- client2/src/app/_data/_apiRoute.ts
+++ client2/src/app/_data/_apiRoute.ts
@@ -1,2 +1,15 @@
 export const _api_login = 'auth/login';
 export const _api_register = 'auth/register';
+
+export const _api_getUser = 'users/getUser';
+export const _api_getFilteredUsers = 'users/getUsers';
+export const _api_updateUser = 'users/updateUser';
+export const _api_deleteUser = 'users/deleteUser';
+
+export const _api_addCert = 'cert/Add';
+export const _api_deleteCert = 'cert/deleteCert';
+export const _api_certList = 'cert/list';
+
+export const _api_newSignUpCounts = 'statistics/newSignupsCount';
+export const _api_signinsCount = 'statistics/signinsCount';
+export const _api_getTrainerViews = 'statistics/trainerViews';
