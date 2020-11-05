761c3d334f1522c1608ee33ec62d7c3060b88e10
astutebytes <73800544+astutebytes@users.noreply.github.com>
2020-11-05 19:22:07 +0500

    Update stats.service.ts

--- client2/src/app/_services/_statsS/stats.service.ts
+++ client2/src/app/_services/_statsS/stats.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { _api_getTrainerViews, _api_newSignUpCounts, _api_signinsCount } from 'src/app/_data/_apiRoute';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatsService {
 
-  constructor() { }
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getNewSignUpCounts(from: string, to: string) {
+
+    const params = new HttpParams().set('from', from.toString()).set('to', to.toString());
+
+    return this.http.get(this.baseUrl + _api_newSignUpCounts, {params});
+  }
+
+  getSigninsCount(from: string, to: string) {
+
+    const params = new HttpParams().set('from', from.toString()).set('to', to.toString());
+
+    return this.http.get(this.baseUrl + _api_signinsCount, {params});
+  }
+
+  getCurrentTrainerViews() {
+    return this.http.get(this.baseUrl + _api_getTrainerViews);
+  }
+
+  getTrainerViewsById(id: number) {
+    return this.http.get(this.baseUrl + _api_getTrainerViews + '/' + id);
+  }
 }
